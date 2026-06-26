171caaeb95bac92182d3cd5c827554fd2699212a
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2026-06-26 20:23:24 +0300

    feat: integrate PrimeNG and update app configuration
    - Added PrimeNG and PrimeUIX themes to package.json.
    - Configured PrimeNG in app.config.ts with Aura theme.
    - Replaced placeholder content in app.html with a PrimeNG button.
    - Updated app.ts to import ButtonModule from PrimeNG.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes), provideClientHydration(),
+
+    // PrimeNG configuration
+    providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+        })
   ]
 };
