0c5442763ac6b8f959c0748c68a7d053da561978
Mahmoud Elshora <131604806+m93-elshora@users.noreply.github.com>
2026-06-30 02:30:54 +0300

    feat: update Why Us section and add fully responsive footer with jira specs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { WhyChooseUs } from "./why-choose-us/why-choose-us";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule],
+  imports: [ButtonModule, WhyChooseUs, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
