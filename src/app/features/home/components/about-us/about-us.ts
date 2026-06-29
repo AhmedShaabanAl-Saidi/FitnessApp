4fd7b0602cdf7f5c916b4ee5b91e779e1fa669c8
ibrahimzaher <hemazaher15@gmail.com>
2026-06-30 01:41:14 +0300

    feat: add about-us section  for home page

---
+++ src/app/features/home/components/about-us/about-us.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Button } from '../../../../shared/button/button';
+
+@Component({
+  selector: 'app-about-us',
+  imports: [Button],
+  templateUrl: './about-us.html',
+  styleUrl: './about-us.css',
+})
+export class AboutUs {
+  content = {
+    sectionTitle: 'About Us',
+    heading: 'EMPOWERING YOU TO ACHIEVE',
+    highlight: 'YOUR FITNESS',
+    headingEnd: 'GOALS',
+    description:
+      "We believe fitness is more than just a workout—it's a lifestyle. With top-of-the-line facilities, certified trainers, and a supportive community, we're here to inspire and guide you every step of the way.",
+    buttonText: 'Get Started',
+    features: [
+      {
+        title: 'Personal Trainer',
+        description: 'Achieve your fitness goals with the guidance of our certified trainers.',
+      },
+      {
+        title: 'Cardio Programs',
+        description: 'From steady-state runs to interval sprints, our treadmill programs.',
+      },
+      {
+        title: 'Quality Equipment',
+        description: 'Our gym is equipped with the latest cardio & strength machines.',
+      },
+      {
+        title: 'Healthy Nutritions',
+        description: 'Fuel your fitness journey with customized meal plans for you.',
+      },
+    ],
+  };
+}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
+import { AboutUs } from './components/about-us/about-us';
 import { Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, AboutUs],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
